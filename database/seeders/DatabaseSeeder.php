c51829a29a6e16633a428a84e962f94a79f643b1
adimov <adimovlliege@gmail.com>
2024-06-14 14:46:14 -0300

    criando usuario pradrão da aplicação

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Dimov',
+            'email' => 'andredimov@hotmail.com',
+            'password' => \Hash::make('123') ,
         ]);
     }
 }
